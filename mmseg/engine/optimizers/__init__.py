81e077ea19bf1c6d6ea4475ef888105fa1155b38
Shuchang Lyu <shuchanglv_ww@163.com>
2025-10-27 18:22:28 +0800

    Add files via upload

---
+++ mmseg/engine/optimizers/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .force_default_constructor import ForceDefaultOptimWrapperConstructor
+from .layer_decay_optimizer_constructor import (
+    LayerDecayOptimizerConstructor, LearningRateDecayOptimizerConstructor)
+from .multi_optimizer_constructor import MultiOptimWrapperConstructor
+
+__all__ = [
+    'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor',
+    'ForceDefaultOptimWrapperConstructor', 'MultiOptimWrapperConstructor'
+]
